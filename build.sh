b6a0d58fb3f4bf54d7577ee0be459de6f8bf2f39
fushun <102985209+fushun891214@users.noreply.github.com>
2025-11-09 23:56:41 +0800

    feat:修改檔案部署方式

--- build.sh
+++ build.sh
@@ -1,2 +1,4 @@
 docker build -t sb2nov/latex .
-docker run --rm -i -v "$PWD":/data sb2nov/latex pdflatex sourabh_bajaj_resume.tex
+docker run --rm -i -v "$PWD":/data sb2nov/latex pdflatex fushun_resume.tex
+mkdir -p resume_pdf
+cp fushun_resume.pdf resume_pdf/
