d0444e994aa13e5d3ac5037427b91988d365d6b6
fushun <102985209+fushun891214@users.noreply.github.com>
2025-11-10 15:07:08 +0800

    feat:修改CV描述與新增中文CV

--- build.sh
+++ build.sh
@@ -1,4 +1,11 @@
 docker build -t sb2nov/latex .
+
+# Build English version with pdflatex
 docker run --rm -i -v "$PWD":/data sb2nov/latex pdflatex fushun_resume.tex
+
+# Build Chinese version with xelatex
+docker run --rm -i -v "$PWD":/data sb2nov/latex xelatex fushun_resume_zh.tex
+
 mkdir -p resume_pdf
 cp fushun_resume.pdf resume_pdf/
+cp fushun_resume_zh.pdf resume_pdf/
